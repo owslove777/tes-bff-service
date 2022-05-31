9a25e1607cf40be1480e534e908adcbc3f95c464
YunKyoung <06007sk@gmail.com>
2022-06-01 02:32:47 +0900

    Resilience4JConfig comment add

--- src/main/java/com/skcc/tes/tesbffservice/Resilience4JConfig.java
+++ src/main/java/com/skcc/tes/tesbffservice/Resilience4JConfig.java
@@ -15,14 +15,14 @@ public class Resilience4JConfig {
     @Bean
     public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
         CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
-                .failureRateThreshold(4)
-                .waitDurationInOpenState(Duration.ofMillis(1000))
-                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-                .slidingWindowSize(2)
+                .failureRateThreshold(4) //실패확률, 100번 중 4번 실패하면 CircuitBreaker open , default 50
+                .waitDurationInOpenState(Duration.ofMillis(1000)) // CircuitBreaker open 유지시간 , default 60 초
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED) // CircuitBreaker closed될때 호출결과기록을 시간/횟수기반으로 할지
+                .slidingWindowSize(2) // CircuitBreaker closed될때 호출결과기록할 창의 크기
                 .build();
 
         TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
-                .timeoutDuration(Duration.ofSeconds(4))
+                .timeoutDuration(Duration.ofSeconds(3)) //호출된 서비스가 3초간 응답 없을경우 CircuitBreaker open
                 .build();
 
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
