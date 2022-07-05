43165b68c6f4b0a4fe9bf198e9693642c09debb3
yongjun <ruzun88@gmail.com>
2022-07-05 23:54:53 +0900

    log added

--- src/main/java/com/skcc/tes/tesbffservice/controller/UserController.java
+++ src/main/java/com/skcc/tes/tesbffservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.skcc.tes.tesbffservice.controller;
 
 import com.skcc.tes.tesbffservice.vo.UserDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController("/data")
 @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.OPTIONS})
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final RestTemplate restTemplate;
@@ -23,6 +25,9 @@ public class UserController {
 
     @GetMapping("/users")
     public List<UserDto> getUsers() {
-        return restTemplate.getForObject(String.format("%s%s",userServiceUrl,"/users"), List.class);
+        log.info("========== redirecting ::: " + userServiceUrl);
+        List list = restTemplate.getForObject(String.format("%s%s", userServiceUrl, "/users"), List.class);
+        log.info("========== result ::: " + list.size());
+        return list;
     }
 }
